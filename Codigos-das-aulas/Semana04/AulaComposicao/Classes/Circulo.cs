2874d0e95f94b0db14f4979135afed159c679ce7
Bruno Costa <brunocf.dev@hotmail.com>
2023-02-23 22:02:40 -0300

    Aula 04 - exercício interfaces

---
+++ Codigos-das-aulas/Semana04/AulaComposicao/Classes/Circulo.cs
@@ -0,0 +1,23 @@
+﻿using AulaComposicao.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaComposicao.Classes
+{
+    public class Circulo : IFiguraGeometrica
+    {
+        public double Raio { get; set; }
+        public double Area()
+        {
+            return 3.14 * (Raio * Raio);
+        }
+
+        public double Perimetro()
+        {
+            return 2 * 3.14 * Raio;
+        }
+    }
+}
---
+++ Codigos-das-aulas/Semana04/AulaComposicao/Classes/Triangulo.cs
@@ -0,0 +1,28 @@
+﻿using AulaComposicao.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaComposicao.Classes
+{
+    public class Triangulo : IFiguraGeometrica
+    {
+        public double ladoA { get; set; }
+        public double ladoB { get; set; }
+        public double ladoC { get; set; }
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public double Area()
+        {
+            return (Base * Altura) / 2;
+        }
+
+        public double Perimetro()
+        {
+            return ladoA + ladoB + ladoC;
+        }
+    }
+}
