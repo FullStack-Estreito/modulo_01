55ea40013e40dcad062c66e09d8092b9dd5ac0dc
Jaime Maretoli <jaimemaretoli@gmail.com>
2023-02-28 01:44:14 -0300

    Add gabaritos dos exercícios 01, 02 e 03 da semana 05 do módulo 01

---
+++ Resolucao-exercicios/semana05/ex3/excecoes/excecoes/Evento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace excecoes
+{
+    public class Evento
+    {
+
+        int qtdLugares;
+        int qtdIngressosVendidos;
+
+        public Evento()
+        {
+        }
+
+        public void ingressoVendido()
+        {
+            Console.WriteLine("Ingresso vendido com sucesso!");
+        }
+
+        public int QtdLugares { get; set; }
+        public int QtdIngressosVendidos { get; set; }
+    }
+}
